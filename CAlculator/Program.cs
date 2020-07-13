345b55e8cbc4cdfa1819deb76d64d2c5f2ee3f15
Avi-cyber <apbusiness5762@gmail.com>
2020-07-13 18:08:37 +0530

    initial commit

---
+++ CAlculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAlculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            char y;
+            Console.WriteLine("enter first number");
+            a = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("enter second number");
+            b = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Which operation do you want to perform");
+            Console.WriteLine(" + " + " * " + " - " + " / ");
+            y = Convert.ToChar(Console.ReadLine());
+            switch (y)
+            {
+                case '+': Console.WriteLine($"The sum of {a} and {b} is {a + b}"); break;
+                case '*': Console.WriteLine($"The multiplication of {a} and {b} is {a * b}"); break;
+                case '-': Console.WriteLine($"The substraction of {a} and {b} is {a - b}"); break;
+                case '/': Console.WriteLine($"The sum of {a} and {b} is {a / b}"); break;
+
+                default: Console.WriteLine("Invalid number");break;
+            }
+        }
+    }
+}
